1cceac2b305b606bf554100c3109d2f497feca6c
Irving Cabello <irvollo@gmail.com>
2020-06-17 16:36:31 -0500

    Add products, mixers and cocktails seeder

---
+++ data/seeds/reset_products_data.js
@@ -0,0 +1,24 @@
+const config = require('../organizations_data');
+
+exports.seed = async (knex) => {
+
+  for (const config_client of config.CLIENTS) {
+      // Get client
+      const client = await knex('clients').where({name: config_client.client_data.name}).first();
+
+      const products = 
+            await knex('products')
+                    .where({client_id: client.id});
+
+      const products_ids = products.map(product => product.id);
+
+      await knex('product_ingredients')
+              .whereIn('product_parent_id', products_ids)
+              .del();
+
+      // Delete products
+      await knex('products')
+              .where({client_id: client.id})
+              .del();
+  }
+};
