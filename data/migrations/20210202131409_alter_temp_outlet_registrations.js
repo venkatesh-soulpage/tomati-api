16841684ac5b6ba431f2a30fa7d947124fc9fe48
preetham-soulpage <preetham.varanasi@soulpageit.com>
2021-02-03 17:57:33 +0530

    Changed account id position

--- data/migrations/20210202131409_alter_temp_outlet_registrations.js
+++ data/migrations/20210202131409_alter_temp_outlet_registrations.js
@@ -1,9 +1,9 @@
 exports.up = function (knex) {
-  return knex.schema.alterTable("temp_outlet_registrations", (table) => {
-    table.integer("plan_id").references("plans.id").alter();
-  });
+  // return knex.schema.alterTable("temp_outlet_registrations", (table) => {
+  //   table.integer("plan_id").references("plans.id").alter();
+  // });
 };
 
 exports.down = function (knex) {
-  knex.schema.dropTable("temp_outlet_registrations");
+  // knex.schema.dropTable("temp_outlet_registrations");
 };
