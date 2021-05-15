c4ecb08e58f7433f17570a734b64af2e24b2fe80
Venkatesh <venkatesh.marreboyina@soulpageit.com>
2021-05-15 19:43:17 +0530

    Updated migration

--- data/migrations/20210514092723_venue_menu_category.js
+++ data/migrations/20210514092723_venue_menu_category.js
@@ -2,7 +2,7 @@ exports.up = function (knex) {
   return knex.schema.createTable("menu_product_categories", (table) => {
     table.increments("id").primary();
     table.integer("menu_product_id").references("outletvenuemenus.id");
-    table.integer("menu_product_category").references("product_category.id");
+    table.integer("menu_product_category").references("product_categories.id");
     table.integer("outlet_venue_id").references("outletvenues.id");
     table.timestamp("created_at").defaultTo(knex.fn.now()).notNullable();
     table.timestamp("updated_at").defaultTo(knex.fn.now()).notNullable();
