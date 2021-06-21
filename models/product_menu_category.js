deecb185ae0b8f7dcf05ec4daef0ca3450441b0c
venkatesh-soulpage <venkatesh.marreboyina@soulpageit.com>
2021-06-21 13:57:18 +0530

    outlet information in menu category

--- models/product_menu_category.js
+++ models/product_menu_category.js
@@ -5,4 +5,16 @@ export default class ProductMenuCategory extends Model {
   static get tableName() {
     return "product_menu_category";
   }
+  static get relationMappings() {
+    return {
+      outlet_venue: {
+        relation: Model.HasOneRelation,
+        modelClass: models.OutletVenue,
+        join: {
+          from: "outletvenues.id",
+          to: "product_menu_category.outlet_venue_id",
+        },
+      },
+    };
+  }
 }
